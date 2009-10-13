6d11e7730c1ec580604bdcce4e23c0de7217ec8d
eddied <eddied@noidea.(none)>
2009-10-13 12:40:58 +0200

    added debug toolbar. removed qa_urls setting

--- broadcastcms_capricornfm/qa_settings.py
+++ broadcastcms_capricornfm/qa_settings.py
@@ -2,6 +2,32 @@ from production_settings import *
 
 DATABASE_NAME += '_qa'
 
-ROOT_URLCONF = 'broadcastcms.lite.qa_urls'
-
 MEDIA_ROOT = '%s/qa_media/' % BUILDOUT_PATH
+
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS = INSTALLED_APPS + (
+    'debug_toolbar',
+)
+
+DEBUG_TOOLBAR_CONFIG = { 
+    'INTERCEPT_REDIRECTS': False 
+}
+
+DEBUG_TOOLBAR_PANELS = (
+    #'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    #'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    #'debug_toolbar.panels.headers.HeaderDebugPanel',
+    #'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    #'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
